2803822b4050014d79353833200fcea0026f1a9c
Eldon Stegall <eldon@eldondev.com>
2020-02-11 03:39:25 +0000

    Add file output for streams

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
+	"regexp"
+	"sync/atomic"
+	"time"
 )
+
+var stream_counter uint64
 
 func main() {
 	ca, _ := loadCA()
@@ -43,17 +49,43 @@ func serve(conn net.Conn, sConfig *tls.Config) {
 	length, err := tlsConn.Read(data)
 	fmt.Println("%d read", length)
 	fmt.Println(tlsConn.ConnectionState().ServerName)
-	outConn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", tlsConn.ConnectionState().ServerName), &tls.Config{KeyLogWriter:os.Stdout})
+	outConn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", tlsConn.ConnectionState().ServerName), &tls.Config{KeyLogWriter: os.Stdout})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer outConn.Close()
+	inFile, outFile := get_record_files(tlsConn.ConnectionState().ServerName)
+	defer inFile.Close()
+	defer outFile.Close()
+	length, err = outFile.Write(data[:length])
+	if err != nil {
+		log.Fatal(err)
+	}
 	length, err = outConn.Write(data[:length])
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	go io.Copy(outConn, tlsConn)
-	io.Copy(tlsConn, outConn)
+	go io.Copy(outConn, io.TeeReader(tlsConn, outFile))
+	io.Copy(tlsConn, io.TeeReader(outConn, inFile))
+}
+
+func get_record_files(serverName string) (inFile, outFile *os.File) {
+	var err error
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileString := regexp.MustCompile("[^[:alnum:]]").ReplaceAllLiteralString(fmt.Sprintf("%s-%s-%d", serverName, time.Now().Format(time.RFC3339Nano), atomic.AddUint64(&stream_counter, 1)), "_")
+	inFile, err = os.Create(fileString + "in")
+	if err != nil {
+		log.Fatal(err)
+	}
+	outFile, err = os.Create(fileString + "out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return inFile, outFile
 }
